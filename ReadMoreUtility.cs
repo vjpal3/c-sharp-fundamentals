77e8ef8a14c89e8956bbe3becb2dfa3a33ddb0eb
vjpal3 <vrishalipal@yahoo.com>
2019-07-27 07:57:00 -0400

    Summarize a block of text

---
+++ ReadMoreUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFundamentals
+{
+    class ReadMoreUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+            Console.WriteLine(text);
+            if (text.Length < maxLength)
+                return text;
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                totalCharacters += word.Length + 1;
+                summaryWords.Add(word);
+                if (totalCharacters >= maxLength)
+                    break;
+            }
+
+            return String.Join(" ", summaryWords) + " ...Read More";
+        }
+    }
+}
