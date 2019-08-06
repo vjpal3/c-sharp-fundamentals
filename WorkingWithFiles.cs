4904a7a02bc9eb856ecd8e29e57495e8fc978fef
vjpal3 <vrishalipal@yahoo.com>
2019-08-05 21:29:33 -0400

    Count words in a file

---
+++ WorkingWithFiles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CsharpFundamentals
+{
+    class WorkingWithFiles
+    {
+       // 1- Write a program that reads a text file and displays the number of words.
+
+        public static int CountWords(string filePath)
+        {
+            int wordCount = 0;
+
+            if(File.Exists(filePath))
+            {
+                var contents = File.ReadAllText(filePath);
+                var lines = contents.Split('\n');
+                foreach(var line in lines)
+                {
+                    foreach (var word in line.Split(' '))
+                    {
+                        wordCount++;
+                    }
+                }
+                return wordCount;
+            }
+
+            return 0;
+
+        }
+    }
+}
