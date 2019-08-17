f5a107fbe4cab55142239308e234b3dfcb62f692
vjpal3 <vrishalipal@yahoo.com>
2019-08-16 20:04:35 -0400

    Card Game - Guess whether the next drawn card is higher or lower than the current card

---
+++ GuessHighLowCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class GuessHighLowCard
+    {
+        public enum Guesses { low, high }
+
+        public static string ProcessRound(Deck deck, string strGuess, Random rand)
+        {
+            var guess = (int)(Guesses)Enum.Parse(typeof(Guesses), strGuess.ToLower());
+            var currentCard = deck.DrawCard(rand);
+            var nextCard = deck.DrawCard(rand);
+
+            Console.WriteLine("Current Card: {0}, {1}", currentCard.Value, currentCard.Suit);
+            Console.WriteLine("Next Card: {0}, {1}", nextCard.Value, nextCard.Suit);
+            Console.WriteLine("Guess: " + strGuess);
+            Console.WriteLine();
+             
+            if (currentCard.Value == nextCard.Value)
+                return "Draw";
+
+            switch (guess)
+            {
+                case 0:
+                    return IsGreater(currentCard, nextCard) ? "Loss" : "Win";
+                case 1:
+                    return IsGreater(currentCard, nextCard) ? "Win" : "Loss";
+            }
+            return "Error";
+        }
+
+        public static bool IsGreater(Card currentCard, Card nextCard)
+        {
+            return nextCard.Value > currentCard.Value;
+        }
+    }
+}
