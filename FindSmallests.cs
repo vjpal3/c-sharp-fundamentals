f70cf0d45af050e845d162c35e327f984955c40d
vjpal3 <vrishalipal@yahoo.com>
2019-09-11 18:48:30 -0400

    Find 3 smallest numbers in a list

---
+++ FindSmallests.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+            var buffer = new List<int>(list);
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                buffer.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+}
