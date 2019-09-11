588dd6ce032b99fd9d237db1a2586e1ab399d6b8
vjpal3 <vrishalipal@yahoo.com>
2019-09-11 19:10:19 -0400

    Covering edge cases for defensive programming

--- FindSmallests.cs
+++ FindSmallests.cs
@@ -7,8 +7,9 @@ namespace CSharpFundamentals
     {
         public static void Main(string[] args)
         {
-            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
-            var smallests = GetSmallests(numbers, 3);
+            var numbers = new List<int> { 1, 2 };
+            //var smallests = GetSmallests(numbers, 3);
+            var smallests = GetSmallests(null, 3);          
 
             foreach (var number in smallests)
                 Console.WriteLine(number);
@@ -16,8 +17,16 @@ namespace CSharpFundamentals
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
-            var smallests = new List<int>();
+            if(list == null)
+                throw new ArgumentNullException("list");
+
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "count should be between 1 and the number of elements in the list");
+
+            //Avoid side effect of this method by copying original list to a buffer. 
             var buffer = new List<int>(list);
+            var smallests = new List<int>();
+            
             while (smallests.Count < count)
             {
                 var min = GetSmallest(buffer);
