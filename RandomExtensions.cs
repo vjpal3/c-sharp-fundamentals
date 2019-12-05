dd2d4b1b2e73083b961ff899b5a7d259af140d53
Vrishali Pal <vrishalipal@yahoo.com>
2019-12-05 12:06:59 -0500

    Add files via upload
    Create class to shuffle the deck

---
+++ RandomExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class RandomExtensions
+    {
+        public static void Shuffle(Random rand, int[] array)
+        {
+            var n = array.Length;
+            while (n > 1)
+            {
+                int k = rand.Next(n--);
+                (array[k], array[n]) = (array[n], array[k]);
+
+            }
+        }
+
+        public static void Shuffle(Random rand, List<int> values)
+        {
+            var n = values.Count;
+            while (n > 1)
+            {
+                int k = rand.Next(n--);
+                (values[k], values[n]) = (values[n], values[k]);
+
+            }
+        }
+
+        public static void Shuffle<T>(Random rand, List<T> list)
+        {
+            var n = list.Count;
+            while (n > 1)
+            {
+                int k = rand.Next(n--);
+                (list[k], list[n]) = (list[n], list[k]);
+
+            }
+        }
+    }
+}
+
