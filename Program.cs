1fc53d657f7a889ed48145395a8e159482739ef1
Vrishali Pal <vrishalipal@yahoo.com>
2019-12-05 11:44:13 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,15 +10,72 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(CardPicker.ProcessRound(new Card("heart", 10), "low"));
+            //var arr = new int[3, 3] {
+            // { 1, 2, 3 },
+            // { 4, 5, 6},
+            // { 7, 8, 9 }
 
+            // };
+            // var sum = 0;
+            // var rowCount = 0;
+            // for (var i = 0; i < 3; i++)
+            // {
+
+            //     var colCount = 0;
+            //     for (var j = 0; j < 3; j++)
+            //     {
+            //         if (!(rowCount == 1 && colCount != 1))
+            //         {
+            //             Console.WriteLine(arr[i, j]);
+            //             sum += arr[i, j];
+            //         }
+
+
+            //         colCount++;
+            //     }
+            //     rowCount++;
+
+            // }
+            // Console.WriteLine(sum);
+
+            //Run the code to play Deck Racer
+            //var rand = new Random();
+            //var racer = new DeckRacer();
+            //Console.WriteLine(racer.StartGame(rand));
+
+            //var racerDeck = new Deck(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+            //var die = new DynamicDie(6);
+            //racerDeck.Shuffle(rand);
+            //racerDeck.ShowDeck();
+            //racerDeck.ValidateDeck(rand);
+            //Console.WriteLine("Showing deck after validating");
+            //racerDeck.ShowDeck();
+
+            //Run the code for D&D Game
+            //var rand = new Random();
+            //var die = new DynamicDie(20);
+            //new DDGame().StartGame(die, 5, rand);
+
+
+            //Run the code to play Yahtzee          
+            //var rand = new Random();
+            //var yahtzee = new Yahtzee(5, 6);
+            //yahtzee.StartGame(3, rand);
+
+            //Run the code for Guessing a card high or low
             //create Random object once to get different random values.
-            Random rand = new Random();            
+            Random rand = new Random();
             var deck = new Deck();
             deck.shuffle(rand);
-            var result = BestHand.FindBestHand(new Hand("Player1", 5, deck, rand), new Hand("Player2", 5, deck, rand));
-            Console.WriteLine(result);
-            Console.WriteLine();
+            Console.WriteLine(GuessHighLowCard.ProcessRound(deck, rand));
+
+            //Run the code for finding best hand
+            //Random rand = new Random();            
+            //var deck = new Deck();
+            //deck.shuffle(rand);
+            //var result = BestHand.FindBestHand(new Hand("Player1", 5, deck, rand), new Hand("Player2", 5, deck, rand));
+            //Console.WriteLine(result);
+            //Console.WriteLine();
         }
     }
 }
